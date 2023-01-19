e9bc6ac447464f2c52b3e48c48cf8e4f15dcae23
goldcollar95 <yjs7298@naver.com>
2023-01-19 18:13:24 +0900

    feat(Lv.0): 옹알이

--- src/main/java/lv0/lv0/옹알이.java
+++ src/main/java/lv0/lv0/옹알이.java
@@ -1,4 +1,33 @@
 package lv0.lv0;
 
 public class 옹알이 {
+    public int solution(String[] babbling) {
+        String[] original = {"aya","ye","ma","woo"};   //제한사항 체크
+        String[] repeat = {"ayaaya", "yeye", "mama", "woowoo"}; //반복사항 체크
+        int answer = 0;
+
+        for(String twokind : babbling){     // String 안에 타입이름 : 매개변수 동시 삽입
+            for(String rbab : repeat){
+                twokind = twokind.replace(rbab, "X");
+            }
+
+            for(String orab : original){
+
+                twokind = twokind.replace(orab, "?");
+            }
+
+            int number = 0;
+            for(int i = 0; i < twokind.length(); i++){
+                if(!twokind.subSequence(i, i+1).equals("?")){
+                    number = 1;
+                    break;
+                }
+            }
+            if(number == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
 }
